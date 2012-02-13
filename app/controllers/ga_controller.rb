cf44029eab0e0fd275e17897eaa88a771f416e7f
Doug Headley <headley.douglas@gmail.com>
2012-02-13 11:53:04 -0800

    removing all cold fusion links.

--- app/controllers/ga_controller.rb
+++ app/controllers/ga_controller.rb
@@ -6,7 +6,8 @@ class GaController < ApplicationController
     @stuff = {}
     @title = "Windermere analytics test report"
     unless params[:report].nil?
-      @results = Ga.query(params[:report])
+      # @results = Ga.query(params[:report])
+      @results = listings_only_filter(params[:report])
       @columns = @results.first.fields
       @listings = WmsSvcConsumer::Models::Listing.find_all_by_agent(params[:report][:agent])
     end
@@ -17,7 +18,7 @@ class GaController < ApplicationController
     @title = "Windermere analytics - agents"
     set_agent_params
     unless params[:profile].nil?
-      @results = Ga.query(params[:profile])
+      @results = listings_only_filter(params[:profile])
       @stuff = params[:profile]
       @start_date = Ga.start_date(params[:profile])
       @end_date = Ga.end_date(params[:profile])
@@ -43,4 +44,17 @@ class GaController < ApplicationController
     sort: "asc"
     }
   end
+  private
+  def listings_only_filter(report)
+    results = Ga.query(report)
+    filtered_results = []
+    results.each do |result|
+      filtered_results << result if result.send(:page_path).match(/\/listing(\/[\w\-]+){4}|\/listings\/(\d{7,})\/gallery(\?refer=map)?/)
+      # filtered_results << result if result.send(:page_path).match(/\/listing(\/[\w\-]+){4}|\/listings/)
+    end
+    # return filtered_results.compact!
+    return filtered_results
+    # return results
+  end
 end
+
