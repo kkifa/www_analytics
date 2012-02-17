a72b7cf8a3b03747a8b340e39e7e43c76ec9b609
Doug Headley <headley.douglas@gmail.com>
2012-02-17 10:26:41 -0800

    Added referral_path to AgentListing report

--- lib/google_analytics/agent_listings.rb
+++ lib/google_analytics/agent_listings.rb
@@ -1,7 +1,7 @@
 module GoogleAnalytics
   class AgentListings
     extend Garb::Model
-    dimensions :page_path, :date
+    dimensions :page_path, :date, :referral_path
     metrics  :pageviews, :unique_pageviews
   end
 end
