2e6089e880d594b419527482c78501b36d879894
Doug Headley <headley.douglas@gmail.com>
2012-02-23 09:03:40 -0800

    updated social media report

--- lib/google_analytics/social_media.rb
+++ lib/google_analytics/social_media.rb
@@ -1,7 +1,7 @@
 module GoogleAnalytics
   class SocialMedia
     extend Garb::Model
-    dimensions  :page_path, :date, :campaign, :source
+    dimensions  :page_path, :date, :campaign, :source, :previous_page_path
     metrics  :pageviews, :unique_pageviews
   end
 end
